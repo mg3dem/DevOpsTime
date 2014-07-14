2cca6dcf8ca4badbaf5939c4aa2ac885cbb3f670
Richard Banasiak <richard@banasiak.com>
2014-07-14 18:16:42 -0500

    Added credit where credit is due.

---
+++ README.md
@@ -0,0 +1,5 @@
+Most of this code is based on research by...
+Tavon Gatling: https://github.com/kentarosu/AndroidWearAndLIFX
+9elements: https://github.com/9elements/9elements-One-for-Android-Wear
+These people: http://www.reddit.com/r/androiddev/comments/297r7m/watch_face_development_on_android_wear/
+Also, these people:  https://plus.google.com/communities/113381227473021565406
--- wear/src/main/java/com/banasiak/android/devopstime/MainActivity.java
+++ wear/src/main/java/com/banasiak/android/devopstime/MainActivity.java
@@ -1,5 +1,11 @@
 package com.banasiak.android.devopstime;
 
+// Most of this code is based on research by...
+// Tavon Gatling: https://github.com/kentarosu/AndroidWearAndLIFX
+// 9elements: https://github.com/9elements/9elements-One-for-Android-Wear
+// These people: http://www.reddit.com/r/androiddev/comments/297r7m/watch_face_development_on_android_wear/
+// Also, these people:  https://plus.google.com/communities/113381227473021565406
+
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
